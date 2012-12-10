2833587d2469101af1236b6b899b53479b271b3f
Graham MacDonald <grahamamacdonald@gmail.com>
2012-12-10 16:13:12 +0000

    e3, initial code for e4

--- src/Euler/core.clj
+++ src/Euler/core.clj
@@ -17,25 +17,73 @@
   (reduce + (multiples-in-range xs limit)))
 
 
+
+
 (defn fib []
   "Lazy fib seq"
   (map first (iterate (fn [[a b]] [b (+ a b)]) [0N 1N])))
 
-(defn e2 [limit]
+(defn sum-of-fibs [limit]
   "Return sum of even fibs <= 4,000,000"
   (reduce + (filter even? (for [x (fib) :while (< x (+ limit 2))] x))))
+
+(def e2 (sum-of-fibs 4000000))
+
 
 
 ; Euclid's algorithm
 (defn gcd [a b]
-  (if (= b 0)
+  (if (zero? b)
     a
     (recur b (mod a b))))
 
+(defn factor? [a pos-factor]
+  (zero? (rem a pos-factor)))
+
+(defn prime? [a]
+  (cond
+   	(< a 0) false
+  	(<= a 2) true
+    :else
+   		(loop [pos-factor (Math/ceil (Math/sqrt a))]
+        (if (<= pos-factor 1)
+          true
+          (if (factor? a pos-factor)
+            false
+            (recur (dec pos-factor)))))))
 
 (defn largest-prime-factor [a]
-  (loop [pos-factor a]
-    (if (= (gcd a pos-factor) 0)
-      pos-factor)
-    (recur (- pos-factor 1))))
+  (loop [pos-factor (int (Math/ceil (Math/sqrt a)))]
+    (if (<= pos-factor 1)
+      0
+      (if (and (factor? a pos-factor) (prime? pos-factor))
+        pos-factor
+        (recur (dec pos-factor))))))
 
+(def e3 (largest-prime-factor 600851475143))
+
+
+
+(defn num-digits [a]
+  (loop [count 0
+         a a]
+    (if (zero? a)
+      count
+      (recur (inc count) (int (/ a 10))))))
+
+(defn digit-at [a digit-idx]
+  (let [num-digits (num-digits a)
+        multiple-of-10 (int (Math/pow 10 (- num-digits digit-idx)))]
+	  (mod (int (/ a multiple-of-10)) 10)))
+
+(defn palindrome? [a]
+  (let [num-digits (num-digits a)]
+  	(loop [idx1 1
+           idx2 num-digits]
+      (if (>= idx1 idx2)
+        true
+        (let [d1 (digit-at a idx1)
+              d2 (digit-at a idx2)]
+          (if (not= d1 d2)
+            false
+            (recur (inc idx1) (dec idx2))))))))
