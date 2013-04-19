044ca843aad0d0e0cbdaa29b534d9b732ae9b80f
gmacd <grahamamacdonald@gmail.com>
2013-04-19 22:45:26 +0100

    e40 done

--- src/Euler/e40.clj
+++ src/Euler/e40.clj
@@ -1,23 +1,16 @@
 ; e40-e41
 (ns Euler.e40
   (:use [Euler.core :as core]
-         [Euler.primes :as primes])
+        [Euler.primes :as primes])
   (:require [clojure.math.combinatorics :as combo]))
 
+
+(defn perms [items]
+  (let [subsets (for [x (range 9 0 -1)] (range x 0 -1))]
+    (reduce concat (map #(combo/permutations %) subsets))))
+
 (defn e40 []
-  (->> (combo/permutations [9 8 7 6 5 4 3 2 1])
+  (->> (perms [9 8 7 6 5 4 3 2 1])
        (map core/digits-to-number)
        (filter odd?)
        (filter primes/prime?)))
-
-(defn perms []
-  (->> (combo/subsets [1 2])
-       (reverse)
-       (map #(flatten (combo/permutations %)))))
-
-;(defn e41 []
-;  (first (filter (comp prime? zeroless-pandigital?)
-;                 (reverse (all-primes 987654321)))))
-
-;(defn e41-test []
-;  (take 10 (primes 987654321))
